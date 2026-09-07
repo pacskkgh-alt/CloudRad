b15807274f61206b992eca9f364993f8782bc448
pacskkgh-alt <pacskkgh@gmail.com>
2026-09-07 19:45:49 +0300

    Fix backend security and pointer

--- backend/api_config.py
+++ backend/api_config.py
@@ -3,8 +3,18 @@ import os
 
 # --- Orthanc PACS ---
 ORTHANC_URL = os.getenv("ORTHANC_URL", "http://cloudrad_orthanc:8042")
-ORTHANC_USER = os.getenv("ORTHANC_USER", "cloudrad_pacs")
-ORTHANC_PASSWORD = os.getenv("ORTHANC_PASSWORD", "CloudR4d_P4cs_Secur3!")
+
+_orthanc_user = os.getenv("ORTHANC_USER")
+_orthanc_password = os.getenv("ORTHANC_PASSWORD")
+
+if not _orthanc_user or not _orthanc_password:
+    raise EnvironmentError(
+        "ORTHANC_USER and ORTHANC_PASSWORD must be set as environment variables. "
+        "Do NOT hardcode PACS credentials in source code."
+    )
+
+ORTHANC_USER: str = _orthanc_user
+ORTHANC_PASSWORD: str = _orthanc_password
 
 # --- Upload Limits ---
 MAX_UPLOAD_SIZE = 2000 * 1024 * 1024  # 2000 MB (2 GB)
